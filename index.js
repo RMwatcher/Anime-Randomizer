19a578234c44e30660dd15fbe42792a3b792a1e3
Richard Mora <llody777@gmail.com>
2026-01-25 21:42:40 -0800

    removed app.post request from index.js

--- index.js
+++ index.js
@@ -4,29 +4,22 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://api.jikan.moe/v4/random/anime?sfw=true";
+const API_URL = "https://api.jikan.moe/v4/";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-
-app.post("/", async (req, res) => {
+app.get("/", async (req, res) => {
     try {
         console.log(rating);
-        const rating = req.body.rating;
-        const response = await axios.get(API_URL + "&rating=" + rating);
-        const result = response.data
+        const result = await axios.get(API_URL + "random/anime?sfw=true");
         console.log(result);
         res.render("index.ejs", {
-            list : result
+            list : result.data.data
         });
     } catch (error) {
         console.error("Failed to make request:", error.message);
-        res.render("index.ejs", {
-            error : "We couldn't process your request at this time. Please try again.",
-        });
+        res.status(500);
     }
 });
 
